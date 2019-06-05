dd882f2b839c92acc40365c3d9c70e2dcd52d228
github username <github email address>
2019-06-05 15:30:38 -0400

    Updates

---
+++ precache-manifest.44c22bc81f249513fdf58b09c46954c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef310298ee05c125dfa76973f7fd0635",
+    "url": "/sight-and-sound-frontend/index.html"
+  },
+  {
+    "revision": "5f058f62c85bef16627f",
+    "url": "/sight-and-sound-frontend/static/css/main.e4d638ca.chunk.css"
+  },
+  {
+    "revision": "c239cd7c42856e7b44c3",
+    "url": "/sight-and-sound-frontend/static/js/2.102844c6.chunk.js"
+  },
+  {
+    "revision": "5f058f62c85bef16627f",
+    "url": "/sight-and-sound-frontend/static/js/main.ec89332d.chunk.js"
+  },
+  {
+    "revision": "e44da88364704f881717",
+    "url": "/sight-and-sound-frontend/static/js/runtime~main.1f395bf5.js"
+  }
+]);
--- src/actions/types.js
+++
@@ -1,3 +0,0 @@
-export const FETCH_VIDEOS = "FETCH_VIDEOS"
-export const FETCH_SONGS = "FETCH_SONGS"
-export const PLAY_SONG = "PLAY_SONG"
